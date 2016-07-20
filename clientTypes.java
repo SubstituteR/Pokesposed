79a8a1c34e8e0152ce8da7e15e6a01b04b1eb244
Riley Strickland <Github@rileystrickland.com>
2016-07-20 07:20:36 -0400

    Add files via upload

---
+++ clientTypes.java
@@ -0,0 +1,10 @@
+package com.rileystrickland.pokesposed;
+
+/**
+ * Created by Riley on 7/20/2016.
+ */
+public class clientTypes {
+    protected static final int Activity = 0;
+    protected static final int Hook = 1;
+    public static final int Public = 2;
+}
---
+++ messageCode.java
@@ -0,0 +1,10 @@
+package com.rileystrickland.pokesposed;
+
+public class messageCode {
+    public static final int MSG_CON = 0;
+    public static final int MSG_DSC = 1;
+    public static final int MSG_POS = 2;
+    public static final int MSG_ADD_LL = 3; //Add ll
+    public static final int MSG_DEL_LL = 4; //Del ll
+    public static final int MGS_CLEAR = 5; //Clear all ll
+}
